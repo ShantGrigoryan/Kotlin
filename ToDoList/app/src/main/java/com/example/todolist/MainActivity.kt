75458f3757f0c9ecc902e6f50b34b3d8d97cb955
Shant <shantgrigorya39@gmail.com>
2024-04-12 18:39:37 +0400

    V1'

---
+++ ToDoList/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.todolist
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val start:TextView = findViewById(R.id.text_getStarted_click)
+
+        start.setOnClickListener{
+            val intent = Intent(this , RegistrationPage::class.java)
+            startActivity(intent)
+        }
+//        start2.setOnClickListener{
+//            Toast.makeText(this@MainActivity, "Email is not valid"
+//                , Toast.LENGTH_LONG).show()
+//        }
+    }
+}
---
+++ ToDoList/app/src/main/java/com/example/todolist/RegistrationPage.kt
@@ -0,0 +1,32 @@
+package com.example.todolist
+
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegistrationPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registration_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    val sign_in: TextView = findViewById(R.id.sign_in_txt)
+    val register: TextView = findViewById(R.id.Register_txt)
+        sign_in.setOnClickListener {
+            Toast.makeText(this@RegistrationPage , "Sign in clicked",
+                Toast.LENGTH_LONG).show()
+        }
+        register.setOnClickListener {
+            Toast.makeText(this@RegistrationPage , "Register clicked",
+                Toast.LENGTH_LONG).show()
+        }
+    }
+}
